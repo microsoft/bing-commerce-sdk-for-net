b828daabf3324b46e03aa54a71a66f14f3fb2b5b
Sohaida Reyad <soreyad@microsoft.com>
2020-06-11 23:05:24 +0200

    add bing commerce alerts sample

---
+++ samples/BingCommerceAlerts/BingCommerceAlerts/Responses/AlertGroupResponse.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BingCommerceAlerts.Responses
+{
+    public class AlertGroupResponse
+    {
+        public AlertGroupSettings AlertGroupSettings { get; set; }
+    }
+
+    public class AlertGroupSettings
+    {
+        public string Enabled { get; set; }
+
+        public string EmailAddress { get; set; }
+    }
+}
---
+++ samples/BingCommerceAlerts/BingCommerceAlerts/Responses/AlertOptionsResponse.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BingCommerceAlerts.Responses
+{
+    public class AlertOptionsResponse
+    {
+        public List<AlertOption> AlertOptions { get; set; }
+    }
+
+    public class AlertOption
+    {
+        public string Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+    }
+}
